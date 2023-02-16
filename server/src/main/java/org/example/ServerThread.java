921dccb1fcc00e7a140705006f7b2571bcc8f9d1
hyuk12 <hotman7025@naver.com>
2023-02-16 11:41:17 +0900

    #4서버작업
    컨트롤러 없애고 서비스 기능을 util로 바꿈

--- server/src/main/java/org/example/ServerThread.java
+++ server/src/main/java/org/example/ServerThread.java
@@ -4,13 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.Socket;
 
+import java.util.ArrayList;
 import java.util.List;
-
-import org.example.controller.ServerController;
 
 import org.example.dto.request.RequestDto;
 import org.example.dto.response.LoginRespDto;
@@ -20,22 +17,24 @@ import org.example.dto.response.LoginRespDto;
 import com.google.gson.Gson;
 
 import lombok.Getter;
+import org.example.util.ServerUtil;
 
 @Getter
 public class ServerThread extends Thread{
-	private List<ServerThread> socketList; 
+	@Getter
+	private static List<ServerThread> socketList = new ArrayList<ServerThread>();
 	private Socket socket;
 	private InputStream inputStream;
 	private Gson gson;
 	
-	private ServerController serverController;
+	private ServerUtil serverService;
 	
 	private String nickname;
 	
 	public ServerThread(Socket socket) {
 		this.socket = socket;
 		this.gson = new Gson();
-		this.serverController = new ServerController(socket);
+		socketList.add(this);
 	}
 
 	@Override
@@ -51,8 +50,8 @@ public class ServerThread extends Thread{
 				
 				switch (requestDto.getResource()) {
 				case "login":
-					LoginRespDto loginRespDto = serverController.loginUser(requestDto);
-					serverController.sendToAll(requestDto.getResource(), "ok", gson.toJson(loginRespDto));
+					LoginRespDto loginRespDto = serverService.loginUser(requestDto);
+					serverService.sendToAll(requestDto.getResource(), "ok", gson.toJson(loginRespDto));
 					break;
 
 				case "message":
---
+++ server/src/main/java/org/example/util/ServerUtil.java
@@ -0,0 +1,59 @@
+package org.example.util;
+
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import lombok.RequiredArgsConstructor;
+import org.example.ServerThread;
+import org.example.dto.request.LoginReqDto;
+import org.example.dto.request.RequestDto;
+import org.example.dto.response.LoginRespDto;
+import org.example.dto.response.ResponseDto;
+
+import com.google.gson.Gson;
+
+import lombok.Getter;
+
+@Getter
+@RequiredArgsConstructor
+public class ServerUtil {
+	private final Gson gson;
+
+	
+	public LoginRespDto loginUser(RequestDto requestDto) {
+
+		LoginReqDto loginReqDto = gson.fromJson(requestDto.getBody(), LoginReqDto.class);
+
+		return new LoginRespDto(loginReqDto.getNickname() + "님이 접속하셨습니다.");
+	}
+	
+	public void sendToAll(String resource, String status, String body) throws IOException {
+		
+		ResponseDto responseDto = new ResponseDto(resource, status, body);
+		
+		for (ServerThread thread: ServerThread.getSocketList()) {
+			OutputStream outputStream = thread.getSocket().getOutputStream();
+			PrintWriter writer = new PrintWriter(outputStream, true);
+			
+			writer.println(gson.toJson(responseDto));
+		}
+	}
+	
+	public void sendToUser(String resource, String status, String body, String toUser) throws IOException {
+		ResponseDto responseDto = new ResponseDto(resource, status, body);
+
+		for(ServerThread thread : ServerThread.getSocketList()) {
+			if(thread.getNickname().equals(toUser)) {
+				OutputStream outputStream = thread.getSocket().getOutputStream();
+				PrintWriter writer = new PrintWriter(outputStream, true);
+
+				writer.println(gson.toJson(responseDto));
+			}
+		}
+	}
+	
+	
+	
+}
